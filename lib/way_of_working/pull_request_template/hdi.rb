2df2717e1389fc9400766046074d3473929c152f
Tim Gentry <52189+timgentry@users.noreply.github.com>
2024-11-01 13:20:45 +0000

    Less brittle require rule

--- lib/way_of_working/pull_request_template/hdi.rb
+++ lib/way_of_working/pull_request_template/hdi.rb
@@ -5,13 +5,11 @@ require_relative 'hdi/paths'
 require 'zeitwerk'
 
 # If way_of_working-audit-github is used we can add a rule
-github_audit_used =
-  begin
-    require 'way_of_working/audit/github/rules/registry'
-  rescue LoadError
-    false
-  end
-require_relative 'hdi/github_audit_rule' if github_audit_used
+begin
+  require 'way_of_working/audit/github/rules/registry'
+  require_relative 'hdi/github_audit_rule'
+rescue LoadError # rubocop:disable Lint/SuppressedException
+end
 
 loader = Zeitwerk::Loader.for_gem_extension(WayOfWorking::PullRequestTemplate)
 loader.ignore("#{__dir__}/hdi/plugin.rb")
