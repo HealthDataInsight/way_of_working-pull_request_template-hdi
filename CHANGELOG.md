a24d64ea3eff8d24853a08dc0661e1e3d6c0c432
Tim Gentry <52189+timgentry@users.noreply.github.com>
2025-11-24 14:34:07 +0000

    Updated GitHub rule signatures

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Changed
 
+- Updated GitHub rule signatures
 - Removed Zeitwerk ignore to make it happier
 
 ## [1.0.0] - 2025-01-24
--- test/way_of_working/audit_github_stub_classes.rb
+++ test/way_of_working/audit_github_stub_classes.rb
@@ -9,22 +9,36 @@ module WayOfWorking
         # This is a simplified version of the base class for GitHub audit rules
         class Base
           attr_accessor :errors, :name, :rulesets, :warnings
+          attr_reader :fix
 
-          def initialize(client, name, repo, rulesets)
+          class << self
+            # Stores and return the source root for this class
+            def source_root(path = nil)
+              @source_root = path if path
+              @source_root ||= nil
+            end
+          end
+
+          def initialize(client, name, repo, rulesets, fix = false)
             @client = client
             @name = name
             @repo = repo
             @repo_name = repo.full_name
             @rulesets = rulesets
+            @fix = fix
             @errors = []
             @warnings = []
           end
 
           def status
             @status ||= begin
-              validate
+              result = validate
 
-              @errors.empty? ? :passed : :failed
+              if result == :not_applicable
+                result
+              else
+                @errors.empty? ? :passed : :failed
+              end
             end
           end
 
@@ -54,17 +68,17 @@ module WayOfWorking
               end
             end
 
-            def rule(rule_name, client, repo)
+            def rule(rule_name, *args)
               klass = Registry.rules.fetch(rule_name, Unknown)
 
-              klass.new(client, repo)
+              klass.new(*args)
             end
           end
         end
 
         # This is a stub handler for rules that aren't in the registry.
         class Unknown < Base
-          def initialize(client, repo_name)
+          def initialize(*)
             super
             raise 'Error: Unknown client'
           end
