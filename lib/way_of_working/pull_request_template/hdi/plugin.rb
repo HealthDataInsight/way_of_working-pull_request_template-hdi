47c86adcb039a751c6695f77803d82905801bcb9
Tim Gentry <52189+timgentry@users.noreply.github.com>
2025-11-24 14:24:51 +0000

    Removed Zeitwerk ignore to make it happier

--- lib/way_of_working/pull_request_template/hdi/plugin.rb
+++ lib/way_of_working/pull_request_template/hdi/plugin.rb
@@ -1,1 +1,13 @@
 require 'way_of_working/pull_request_template/hdi'
+
+# Add any plug specific code here
+
+module WayOfWorking
+  module PullRequestTemplate
+    module Hdi
+      # This makes zeitwerk happy, which makes us happy too
+      module Plugin
+      end
+    end
+  end
+end
